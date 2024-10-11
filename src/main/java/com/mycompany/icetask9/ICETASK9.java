bb16d8c00bb3e257d1e44de7ea6387c921b26e10
RC_Student_lab <RC_Student_lab@labG9AEB3.phdvvtwzx4oudioi2mg3lhtmwg.fx.internal.cloudapp.net>
2024-10-11 19:10:46 +0000

    Ice task

---
+++ src/main/java/com/mycompany/icetask9/ICETASK9.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.icetask9;
+import java.util.Scanner;
+import javax.swing.*;
+/**
+ *
+ * @author RC_Student_lab
+ */
+public class ICETASK9 {
+
+    public static void main(String[] args) {
+     Scanner in = new Scanner(System.in);   
+        
+    
+        //System.out.println(name);
+        int a, b;
+        String num1, num2;
+ 
+       num1 = JOptionPane.showInputDialog(null,"Input 1st number");
+       a = Integer.parseInt(num1);
+       
+        num2 = JOptionPane.showInputDialog(null,"Input 2nd number");
+        b = Integer.parseInt(num2);
+         
+      //System.out.println("sum = "+(a+b));
+      while (true){
+        String menu = "======Menu=======\n"
+   +"1. addition\n"
+   +"2. subtration\n"
+   +"3. division\n"
+   +"4.multiplication\n"
+   +"0 Exit";           
+                
+    
+String choice = JOptionPane.showInputDialog(menu);            
+        
+ 
+ 
+      switch (choice){
+          case "1": JOptionPane.showInputDialog(null," The sum of " + a + " & " + b + " is " + ( a + b ) );
+      break;
+          case "2":
+              JOptionPane.showInputDialog(null," The difference" + a + " & " + b + " is " + ( a - b ) );
+         break;    
+       case "3":
+              JOptionPane.showInputDialog(null," The product of " + a + " & " + b + " is " + ( a * b ) );
+         break;
+       case "0": 
+               JOptionPane.showInputDialog(null, "Exiting...");
+               System.exit(0);
+               break;
+      
+      }
+        
+      }
+   
+        
+        
+    }
+}
